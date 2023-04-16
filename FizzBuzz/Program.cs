74d1132a3e7b0ac73dee4a4d5a5a1dd804a221bd
Malik F. Hayat <4572745+codingj@users.noreply.github.com>
2023-04-16 22:55:20 +0200

    Update Program.cs

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const int countTo = 100;
+FizzBuzzCalc(1, countTo);
+
+void FizzBuzzCalc(int number, int count)
+{
+    var divisbleByThree = IsDivisble(number, 3, "Fizz");
+    var divisbleByFive = IsDivisble(number, 5, "Buzz");
+    
+    var shoutOut = !string.IsNullOrEmpty(divisbleByThree) || !string.IsNullOrEmpty(divisbleByFive) ? $"{divisbleByThree}{divisbleByFive}" : number.ToString();
+    Console.WriteLine(shoutOut);
+
+    if (number < count) FizzBuzzCalc(number+1, count);
+}
+
+string IsDivisble(int number, int divider, string text)
+{
+    return number % divider == 0 ? text : string.Empty;
+}
